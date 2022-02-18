47929d40ff1373df87068db0fb2a5de845fa3b50
Zachary Cecchetti <zcecchetti@gmail.com>
2022-02-17 21:38:33 -0800

    Wrote operator functions and operate function to call numbers and operator functions

---
+++ javascript.js
@@ -0,0 +1,31 @@
+
+function add (num1, num2) {
+
+    return result = num1 + num2;
+};
+
+function subtract (num1, num2) {
+
+    return result = num1 - num2;
+};
+
+function divide (num1, num2) {
+
+    return result = num1/num2;
+};
+
+function multiply (num1, num2) {
+
+    return result = num1 * num2;
+};
+
+function powerOf (num1, num2) {
+
+    return result = num1 ** num2;
+};
+
+function operate (num1, operator, num2) {
+
+    let answer = operator(num1, num2);
+    return answer;
+};
