eb10141ecff791381079868ec72ee8c66d3d401f
Zachary Cecchetti <zcecchetti@gmail.com>
2022-02-18 23:34:57 -0800

    Continued building calculator design

--- javascript.js
+++ javascript.js
@@ -23,6 +23,10 @@ function remainderOf (num1, num2) {
     return result = num1 % num2;
 };
 
+function negativeOf (num1) {
+    return result = 0 - num1;
+};
+
 function operate (num1, operator, num2) {
     let answer = operator(num1, num2);
     return answer;
